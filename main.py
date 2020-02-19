ce5a78e06ef5e367459bd7012ee1ca6cea11be3e
SergeyR <rozanovsa@gmail.com>
2020-02-19 09:30:09 +0300

    Added main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 def game_core_v2(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его
        в зависимости от того, больше оно или меньше нужного.
@@ -11,6 +12,17 @@ def game_core_v2(number):
         elif number < predict: 
             predict -= 1
     return(count) # выход из цикла, если угадали
+
+def score_game(game_core_v1):
+    '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core_v1(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
 
 # Проверяем
 score_game(game_core_v2)
