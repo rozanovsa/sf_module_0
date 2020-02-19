e8b0b5097b902f761bf039cc33b16df72a5b6a11
SergeyR <rozanovsa@gmail.com>
2020-02-19 09:10:03 +0300

    Added main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его
+       в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    predict = np.random.randint(1,100)
+    while number != predict:
+        count+=1
+        if number > predict: 
+            predict += 1
+        elif number < predict: 
+            predict -= 1
+    return(count) # выход из цикла, если угадали
+
+# Проверяем
+score_game(game_core_v2)
